3785a862aa6ea3f2ba73b513d41e0d03c07b2af7
Keith Higa <keith.higa@gmail.com>
2019-01-12 16:30:43 -1000

    Change paths for static and database files

--- earc2/settings.py
+++ earc2/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../public/static'))
 
 
 # Email settings offloaded to gmail_config.py
