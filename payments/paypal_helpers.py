a9a213d8d12c651f11c61a8db062e8d301c525cf
Keith Higa <keith.higa@gmail.com>
2019-01-26 19:25:24 -1000

    Added exception handling for dev if ngrok not active

--- payments/paypal_helpers.py
+++ payments/paypal_helpers.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from requests.exceptions import ConnectionError
 from earc2.settings import PAYPAL_TEST
 
 def paypal_email_test_or_prod():
@@ -14,8 +15,13 @@ def paypal_email_test_or_prod():
 		
 def get_ngrok_url():
 	url = "http://localhost:4040/api/tunnels"
-	res = requests.get(url)
+	try:
+		res = requests.get(url)
+	except ConnectionError as e:
+		print("Warning: ngrok not activated. PayPal IPN will not function correctly.")
+		return "http://localhost:8000"
 	res_unicode = res.content.decode("utf-8")
 	res_json = json.loads(res_unicode)
 	return res_json["tunnels"][1]["public_url"]
+
 	
