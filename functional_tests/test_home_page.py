1e50654aadff8c973f4a7f29b52a76994cceda5a
pineapplejuice <keith.higa@gmail.com>
2019-07-24 20:58:40 -1000

    Move static and base templates to manage_members

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -10,24 +10,45 @@ MAX_WAIT = 10
 
 class TestHomePage(FunctionalTest):
 
-	def test_home_page_display(self):
-		# Keith goes to home page
-		self.browser.get(self.live_server_url)
-		
-		# He sees the welcome screen to the home page
-		self.assertIn('Member Portal', self.browser.title)
-		header_text = self.browser.find_element_by_class_name('h2').text
-		self.assertIn('Member Portal', header_text)
-	
-	def test_log_in(self):
-		# Keith goes to home page
-		self.browser.get(self.live_server_url)
-		
-		# He wants to log in, click the login link
-		self.browser.find_element_by_link_text('Login').click()
-		element = WebDriverWait(self.browser, 10).until(
-			EC.presence_of_element_located((By.ID, "id_username")))
-		self.assertIn("Member Login", self.browser.find_element_by_id('title').text)
-		
-
-		
+    def test_home_page_display(self):
+        # Keith goes to home page
+        self.browser.get(self.live_server_url)
+        
+        # He sees the welcome screen to the home page
+        self.assertIn('Member Portal', self.browser.title)
+        header_text = self.browser.find_element_by_id('title').text
+        self.assertIn('Member Portal', header_text)
+    
+    def test_log_in(self):
+        # Keith goes to home page
+        self.browser.get(self.live_server_url)
+        
+        # He wants to log in, click the login link
+        self.browser.find_element_by_link_text('Login').click()
+        element = WebDriverWait(self.browser, MAX_WAIT).until(
+            EC.presence_of_element_located((By.ID, "id_username")))
+        self.assertIn("Member Login", self.browser.find_element_by_id('title').text)
+        
+    def test_new_member_button_from_home_page(self):
+        self.browser.get(self.live_server_url)
+        
+        self.browser.find_element_by_id('new_member_btn').click()
+        element = WebDriverWait(self.browser, MAX_WAIT).until(
+            EC.title_contains("Membership Application"))
+        self.assertIn("Membership Application", self.browser.find_element_by_id('title').text)
+    
+    def test_login_button_from_home_page(self):
+        self.browser.get(self.live_server_url)
+       
+        self.browser.find_element_by_id('login_btn').click()
+        element = WebDriverWait(self.browser, MAX_WAIT).until(
+            EC.title_contains("Member Login"))
+        self.assertIn("Member Login", self.browser.find_element_by_id('title').text)
+    
+    def test_member_list_from_home_page(self):
+        self.browser.get(self.live_server_url)
+       
+        self.browser.find_element_by_id('member_list_btn').click()
+        element = WebDriverWait(self.browser, MAX_WAIT).until(
+            EC.title_contains("Member List"))
+        self.assertIn("Member List", self.browser.find_element_by_id('title').text)
